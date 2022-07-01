c31cd5038c9bdbee384b6157b6503aefe8165e4d
Edu <particularspam@gmail.com>
2022-07-01 01:47:48 -0500

    random cats button

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ console.log("Hello world");
 
 const URL = "https://api.thecatapi.com/v1/images/search";
 
-fetch(URL)
-  .then((res) => res.json())
-  .then((data) => {
-    const img = document.querySelector("img");
-    img.src = data[0].url;
-  });
+async function myCat() {
+  const res = await fetch(URL);
+  const data = await res.json();
+  const img = document.querySelector("img");
+  img.src = data[0].url;
+}
+
+const myButton = document.querySelector("button");
+myButton.onclick = myCat;
