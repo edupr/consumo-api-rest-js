4dc4bed8c4aa70ab2df8838047df726a13e9d6d0
Edu <particularspam@gmail.com>
2022-07-02 02:52:17 -0500

    asyn await fect, query parameters

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 console.log("Hello world");
 
-const URL = "https://api.thecatapi.com/v1/images/search";
+const API_URL = "https://api.thecatapi.com/v1/images/search?limit=3";
 
-async function myCat() {
-  const res = await fetch(URL);
+// async function myCat() {
+//   const res = await fetch(URL);
+//   const data = await res.json();
+//   const img = document.querySelector("img");
+//   img.src = data[0].url;
+// }
+// const myButton = document.querySelector("button");
+// myButton.onclick = myCat;
+
+// Fetch data con async await
+
+async function reload() {
+  const res = await fetch(API_URL);
   const data = await res.json();
-  const img = document.querySelector("img");
-  img.src = data[0].url;
+
+  console.log(data);
+  const img1 = document.getElementById("img1");
+  const img2 = document.getElementById("img2");
+  const img3 = document.getElementById("img3");
+
+  img1.src = data[0].url;
+  img2.src = data[1].url;
+  img3.src = data[2].url;
 }
 
-const myButton = document.querySelector("button");
-myButton.onclick = myCat;
+reload();
