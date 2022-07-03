ab5a4cb5e5286bc6f69d90b280418378f7bb350d
Edu <particularspam@gmail.com>
2022-07-03 15:11:16 -0500

    add favourite cat

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ const API_URL_RANDOM = [
 
 const API_URL_FAVOURITES = [
   "https://api.thecatapi.com/v1/favourites",
-  "?limit=2",
-  "&order=Asc",
-  `&api_key=${API_KEY}`,
+  // "?limit=2",
+  // "&order=Asc",
+  `?api_key=${API_KEY}`,
 ].join("");
 
 const spanError = document.getElementById("error");
@@ -40,8 +40,12 @@ async function loadRandomMichis() {
   {
     const img1 = document.getElementById("img1");
     const img2 = document.getElementById("img2");
+    const btn1 = document.getElementById("btn1");
+    const btn2 = document.getElementById("btn2");
     img1.src = data[0].url;
     img2.src = data[1].url;
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
   }
 }
 
@@ -53,17 +57,32 @@ async function loadFavouriteMichis() {
   if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status;
     console.log("Hubo un error: ", res.status, data.message);
+  } else {
+    data.forEach((michi) => {
+      const section = document.getElementById('favouriteMichis');
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar gato de favoritos');
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
   }
 }
 
-async function saveFavourite() {
+async function saveFavouriteMichi(id) {
   const res = await fetch(API_URL_FAVOURITES, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      image_id: "tde",
+      image_id: id,
     }),
   });
 
