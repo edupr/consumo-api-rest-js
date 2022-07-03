c57314f9efb3f6dc2c716dca20fdad299c6f86c0
Edu <particularspam@gmail.com>
2022-07-03 11:38:52 -0500

    status errors

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-console.log("Hello world");
-
 const API_KEY = "648a3f25-fd2f-41cc-be40-d17b46a00fd1";
 
-const API_URL = [
+const API_URL_RANDOM = [
   "https://api.thecatapi.com/v1/images/search",
-  "?limit=3",
-  "&order=Asc",
+  "?limit=2",
   `&api_key=${API_KEY}`,
 ].join("");
 
-querystring = ["?", "limit=3", "&order=Desc"].join("");
+const API_URL_FAVOURITES = [
+  "https://api.thecatapi.com/v1/favourites",
+  "?limit=2",
+  "&order=Asc",
+  // `&api_key=${API_KEY}`,
+].join("");
+// const API_URL_FAVOURITES = [
+//   "https://api.thecatapi.com/v1/favourites",
+//   "?limit=2",
+//   "&order=Asc",
+//   `&api_key=${API_KEY}`,
+// ].join("");
+
+const spanError = document.getElementById("error");
+
+querystring = ["?", "limit=2", "&order=Desc"].join("");
 
 // const API_URL = `https://api.thecatapi.com/v1/images/search${querystring}`;
 
@@ -21,21 +33,33 @@ querystring = ["?", "limit=3", "&order=Desc"].join("");
 // }
 // const myButton = document.querySelector("button");
 // myButton.onclick = myCat;
-
 // Fetch data con async await
 
-async function reload() {
-  const res = await fetch(API_URL);
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
+  console.log("Random: ", data);
 
-  console.log(data);
-  const img1 = document.getElementById("img1");
-  const img2 = document.getElementById("img2");
-  const img3 = document.getElementById("img3");
-
-  img1.src = data[0].url;
-  img2.src = data[1].url;
-  img3.src = data[2].url;
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  }
+  {
+    const img1 = document.getElementById("img1");
+    const img2 = document.getElementById("img2");
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+  }
 }
 
-reload();
+async function loadFavoritesMichis() {
+  const res = await fetch(API_URL_FAVOURITES);
+  const data = await res.json();
+  console.log("Favorites: ", data);
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  }
+}
+
+loadRandomMichis();
+loadFavoritesMichis();
